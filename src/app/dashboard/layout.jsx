51f59616616caa09431ac44a4027c6dbdd867c52
cyberdavinci <ebrimaa2ray@gmail.com>
2023-09-15 08:04:35 -1200

    removed env

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -2,16 +2,21 @@
 import React, { useState } from "react";
 // import styles from "./page.module.css";
 import DashboardNav from "@/components/dashboardNav/DashboardNav";
+import { motion } from "framer-motion";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 const layout = ({ children }) => {
   const [open, setOpen] = useState("");
+  const router = useRouter();
+  const session = useSession();
   const toggleMenu = () => {
     setOpen((prev) => (prev === "" ? "open" : ""));
   };
   return (
-    <div>
-      <DashboardNav open={open} toggleMenu={toggleMenu} />
-      {children}
-    </div>
+    <motion.div className="relative">
+      <DashboardNav />
+      <div className=" float-right ">{children}</div>
+    </motion.div>
   );
 };
 
--- src/components/dashboardNav/DashboardNav.jsx
+++ src/components/dashboardNav/DashboardNav.jsx
@@ -2,66 +2,94 @@
 import React, { useEffect, useState } from "react";
 import styles from "./dashboardNav.module.css";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { RiMenuUnfoldLine } from "react-icons/ri";
+import { FiSettings } from "react-icons/fi";
+import { PiStudentDuotone } from "react-icons/pi";
+import { MdOutlineGeneratingTokens } from "react-icons/md";
+import { GiBookshelf } from "react-icons/gi";
+import { BiHomeAlt } from "react-icons/bi";
 const DashboardNav = () => {
-  const [open, setOpen] = useState("");
+  const [open, setOpen] = useState("open");
   //   useEffect(() => {}, [open]);
   const links = [
     {
       customS: {
         "--i": 1,
       },
-      name: "H",
+      name: "Dashboard",
+      icon: <BiHomeAlt />,
+      page: "/dashboard",
     },
     {
       customS: {
         "--i": 2,
       },
-      name: "S",
+      name: "Settings",
+      icon: <FiSettings />,
+      page: "/dashboard/settings",
     },
     {
       customS: {
         "--i": 3,
       },
-      name: "T",
+      name: "Students",
+      icon: <PiStudentDuotone />,
+      page: "/dashboard/students",
     },
     {
       customS: {
         "--i": 4,
       },
-      name: "S",
+      name: "Tokens",
+      icon: <MdOutlineGeneratingTokens />,
+      page: "/dashboard/tokens",
     },
     {
       customS: {
         "--i": 5,
       },
-      name: "SJ",
+      name: "Subjects",
+      icon: <GiBookshelf />,
+      page: "/dashboard/subjects",
     },
   ];
   const toggleMenu = () => {
-    // setOpen((prev) => (prev === "" ? "open" : ""));
-    // setOpen("open");
-    console.log("open");
+    // console.log("open");
     open === "open" ? setOpen("") : setOpen("open");
   };
   return (
-    <div>
+    <motion.div
+      className=" float-left flex-1"
+      drag
+      dragConstraints={{
+        top: -50,
+        left: -50,
+        right: 50,
+        bottom: 150,
+      }}
+    >
       <nav className={`${styles.nav} ${open === "open" ? styles.open : null}`}>
         <div className={styles.toggle_btn} onClick={() => toggleMenu()}>
-          <i className={`${styles.bx} ${styles.bx_plus}`}>+</i>
+          <RiMenuUnfoldLine />
         </div>
         {links.map((link, index) => {
           return (
             <div className={styles.nav_content} key={index}>
               <span style={link.customS}>
-                <Link href="#">
-                  <i>{link.name}</i>
+                <Link href={link.page}>
+                  <i>
+                    {link.icon}
+                    {/* {link.name} */}
+                  </i>
+                  {/* <p> {link.name} </p> */}
                 </Link>
               </span>
             </div>
           );
         })}
       </nav>
-    </div>
+    </motion.div>
   );
 };
 
