d99adcef59c48a84bcac140a7962083dd775f04a
cyberdavinci <ebrimaa2ray@gmail.com>
2023-11-23 22:42:01 -1200

    updates

--- src/components/excelsheet/Sheet.js
+++ src/components/excelsheet/Sheet.js
@@ -4,20 +4,33 @@ import React, { useState } from "react";
 import { read, utils, writeFileXLSX } from "xlsx";
 export const ExportExcelButton = ({ data, selectedSubs }) => {
   const subs = Array.from(selectedSubs);
+  // console.log(selectedSubs);
   const newData = () => {
     // well... that's that.
     /*
     subjects are in an array and when we export the json data some how the subjects column in our sheet returns empty, so I decided to convert the array to string first, then remove all keys in object that are irrelivant
     */
-    //  const subs = ar
     const subjectsArrayToString = data?.map((element) => {
       const filteredElement = Object?.keys(element)
-        .filter((key) => key !== "_id" && key !== "route" && key !== "status")
+        .filter(
+          (key) =>
+            key !== "_id" &&
+            key !== "route" &&
+            key !== "status" &&
+            key !== "_v" &&
+            key !== "resetToken"
+        )
         .reduce((obj, key) => {
           obj[key] = element[key];
           return obj;
         }, {});
-      return { ...filteredElement, subjects: subs?.toString()?.toUpperCase() };
+      return {
+        ...filteredElement,
+        subjects:
+          selectedSubs !== "all"
+            ? subs?.toString()?.toUpperCase()
+            : filteredElement?.subjects?.toString()?.toUpperCase(),
+      };
     });
     // const finalData = subjectsArrayToString.filter()
     // console.log(subjectsArrayToString);
