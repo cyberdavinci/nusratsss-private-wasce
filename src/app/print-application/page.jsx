895527e46e949d9a4115ac859e92e21dc1ccc326
cyberdavinci <ebrimaa2ray@gmail.com>
2023-12-07 20:44:49 +0000

    fixed blank page print

--- src/app/print-application/page.jsx
+++ src/app/print-application/page.jsx
@@ -1,6 +1,7 @@
+"use client";
 import React from "react";
 import PrintApplication from "../../components/PrintPage/PrintApplication";
-const page = () => {
+const PrintAppForm = () => {
   // console.log(student);
   return (
     <div className="h-screen bg-white overflow-y-auto">
